2a40b546ed7c2a58ff3c09c8141af8a6ae95fb63
Marie Crumpton <marie.k.cr@gmail.com>
2020-11-18 17:53:34 -0500

    beginning set up of site

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+# Marie Crumpton's Portfolio Site ☀️
+
+## About
+
+Hello, my name is Marie. I'm a teacher turned software engineer. This is my portfolio website to show off my projects and still in progress projects!
+
+Pop on over to http://www.mariecrumpton.dev/
+
+## Acknowledgement
+
+Created using the [Gatsby Simplefolio](https://github.com/cobidev/gatsby-simplefolio) by [Jacob Martinez](https://github.com/cobidev)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
 # Gatsby Simplefolio ⚡️ [![GitHub](https://img.shields.io/github/license/cobidev/gatsby-simplefolio?color=blue)](https://github.com/cobidev/gatsby-simplefolio/blob/master/LICENSE.md) ![GitHub stars](https://img.shields.io/github/stars/cobidev/gatsby-simplefolio) ![GitHub forks](https://img.shields.io/github/forks/cobidev/gatsby-simplefolio)
 
 ## A clean, beautiful and responsive portfolio template for Developers
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Marie Crumpton', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
@@ -10,9 +10,9 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  name: 'Marie Crumpton',
+  subtitle: 'teacher turned software engineer',
+  cta: 'learn more',
 };
 
 // ABOUT DATA
@@ -28,67 +28,58 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'gobARk.jpg',
+    title: 'go.bARk',
+    info: 'what the game is all ',
+    info2: 'about ',
+    url: 'https://www.youtube.com/watch?v=tJig6T0Ccoc',
+    repo: 'https://github.com/gh-capstone-team-c/Go.bARk',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'SeeTurtleExploration.jpg',
+    title: 'SeeTurtle Exploration',
+    info: 'What the game',
+    info2: 'is all about ',
+    url: 'https://www.youtube.com/watch?v=dUDTRJXvqfY',
+    repo: 'https://github.com/mkcrumpton/SeeTurtleExploration',
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: "Let's Chat",
+  email: 'marie.k.cr@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+    // {
+    //   id: nanoid(),
+    //   name: 'twitter',
+    //   url: '',
+    // },
+    // {
+    //   id: nanoid(),
+    //   name: 'codepen',
+    //   url: '',
+    // },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/mariecrumpton/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/mkcrumpton',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
